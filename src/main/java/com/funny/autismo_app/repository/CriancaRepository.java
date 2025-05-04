358eb22c83d64fb909a5a2b93dba9204b315610e
victoriaUNI <victoria.00000851663@unicap.br>
2025-05-04 21:32:17 +0000

    restart

--- src/main/java/com/funny/autismo_app/repository/CriancaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.funny.autismo_app.repository;
-
-import com.funny.autismo_app.model.Crianca;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CriancaRepository extends JpaRepository<Crianca, Long> {
-}
--- src/main/java/com/funny/autismo_app/repository/ResponsavelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.funny.autismo_app.repository;
-
-import com.funny.autismo_app.model.Responsavel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResponsavelRepository extends JpaRepository<Responsavel, Long> {
-}
